a6d2b89994fdcfb0d64ca64b07300de0485e7941
Zheng Ma <zhengma@sjtu.edu.cn>
2025-10-05 21:46:06 +0800

    fix(Header): update alt attribute of logo image from "Persona" to "Logo"

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,4 @@
 import mdx from '@astrojs/mdx'
-import netlify from '@astrojs/netlify'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
@@ -13,5 +12,4 @@ export default defineConfig({
   vite: {
     plugins: [tailwindcss()],
   },
-  adapter: netlify(),
 })
