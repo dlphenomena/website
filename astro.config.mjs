c7ba0de06c61fba3840feb305e737b470a489e9a
Zheng Ma <zhengma@sjtu.edu.cn>
2025-10-07 19:30:23 +0800

    feat: add reading time and last modified time plugins for blog posts

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,14 @@ import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'astro/config'
+import { remarkReadingTime } from './src/plugins/remark-reading-time.mjs'
+import { remarkModifiedTime } from './src/plugins/remarkModifiedTime.mjs'
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://dlphenomena.netlify.app',
   integrations: [sitemap(), react(), mdx()],
-  markdown: {},
+  markdown: { remarkPlugins: [remarkReadingTime, remarkModifiedTime] },
   vite: {
     plugins: [tailwindcss()],
   },
