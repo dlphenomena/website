52d44017a5a7d5df3f28842ba736ef4fc1dbcf66
Zheng Ma <mazhengcn@outlook.com>
2025-10-31 14:42:57 +0800

    Switch to useLayoutEffect and resolvedTheme

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -3,7 +3,7 @@
 import { cn } from "@/lib/utils"
 import { cva } from "class-variance-authority"
 import { useTheme } from "next-themes"
-import { Activity, useEffect, useState } from "react"
+import { Activity, useLayoutEffect, useState } from "react"
 import { IoMoon, IoSunny } from "react-icons/io5"
 
 const itemVariants = cva("cursor-pointer rounded-3xl p-2", {
@@ -18,35 +18,31 @@ const itemVariants = cva("cursor-pointer rounded-3xl p-2", {
 const themes = ["light", "dark"]
 
 export default function ThemeToggle() {
-  const { theme, setTheme } = useTheme()
+  const { setTheme, resolvedTheme } = useTheme()
   const [mounted, setMounted] = useState(false)
 
   // Set mounted on first render
-  useEffect(() => {
+  useLayoutEffect(() => {
     // eslint-disable-next-line
     setMounted(true)
   }, [])
 
-  const toggleTheme = () => {
-    const t = theme === "light" ? "dark" : "light"
-    setTheme(t)
-  }
+  const value = mounted ? resolvedTheme : null
 
   return (
     <Activity mode={mounted ? "visible" : "hidden"}>
       <div className="inline-flex items-center rounded-3xl bg-orange-300 p-px dark:bg-zinc-600">
-        {themes.map((t) => {
-          const active = t === theme
+        {themes.map((key) => {
           return (
             <button
-              key={t}
+              key={key}
               type="button"
-              className={cn(itemVariants({ active: active }))}
-              onClick={toggleTheme}
+              className={cn(itemVariants({ active: key === value }))}
+              onClick={() => setTheme(value === "light" ? "dark" : "light")}
               aria-label="Toggle theme"
               data-theme-toggle=""
             >
-              {t === "light" ? <IoSunny /> : <IoMoon />}
+              {key === "light" ? <IoSunny /> : <IoMoon />}
             </button>
           )
         })}
